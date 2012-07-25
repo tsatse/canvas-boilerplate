28792e19ff108a5f9dbbe2df26f9f64143fe816e
unknown <djo@pxv_001.(none)>
2012-07-25 19:52:16 +0200

    mouse and keyboard and fixes

--- canvas-boiler-plate.js
+++ canvas-boiler-plate.js
@@ -1,49 +1,33 @@
-﻿function sketch(setup) {
-    var defaults = {
-        width: window.innerWidth,
-        height: window.innerHeight
-    };
-    if (!Function.prototype.bind) {
-      Function.prototype.bind = function (oThis) {
-        if (typeof this !== "function") {
-          // closest thing possible to the ECMAScript 5 internal IsCallable function
-          throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-        }
-
-        var aArgs = Array.prototype.slice.call(arguments, 1),
-            fToBind = this,
-            fNOP = function () {},
-            fBound = function () {
-                try {
-              return fToBind.apply(this instanceof fNOP
-                                     ? this
-                                     : oThis,
-                                   aArgs.concat(Array.prototype.slice.call(arguments)));
-                } catch(e) {
-                    //catching what javascriptcore considers an illegal use of instanceof
-                    return fToBind.apply(oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
-                }
-            };
-
-        fNOP.prototype = this.prototype;
-        fBound.prototype = new fNOP();
-
-        return fBound;
-      };
-    }    
-
-    var canvas = setup.canvas || (function() { return document.body.appendChild(document.createElement('canvas')); }());
-
-    canvas.width = setup.width || defaults.width;
-    canvas.height = setup.height || defaults.height;
-
-    var ctx = canvas.getContext('2d');
-
-    var fps = 60;
-    if(setup.fps) {
-        fps = setup.fps;
+﻿if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
     }
-    var lastDraw = null;
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+            try {
+          return fToBind.apply(this instanceof fNOP
+                                 ? this
+                                 : oThis,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+            } catch(e) {
+                //catching what javascriptcore considers an illegal use of instanceof
+                return fToBind.apply(oThis, aArgs.concat(Array.prototype.slice.call(arguments)));
+            }
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}    
+
+function sketch(setup) {
     var reqAnimFrame = (window.requestAnimationFrame       || 
                        window.webkitRequestAnimationFrame || 
                        window.mozRequestAnimationFrame    || 
@@ -52,53 +36,101 @@
                         function(callback) {
                             window.setTimeout(callback, 1000 / fps);
                         }).bind(window);
+    var defaults = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        fps: 60,
+        update: function() {},
+        draw: function() {},
+        resetState: function(state) {return state;},
+        state: {},
+        events: {
+            loaded: function() {},
+            progress: function() {}
+        }
+    };
 
+    var canvas = setup.canvas || (function() { return document.body.appendChild(document.createElement('canvas')); }());
+    canvas.width = setup.width || defaults.width;
+    canvas.height = setup.height || defaults.height;
+    var pointerInput = new PointerInput();
+    pointerInput.attach(canvas);
+    var pointerCallbacks = [
+        'down-left',
+        'down-middle',
+        'down-right',
+        'up-left',
+        'up-middle',
+        'up-right',
+        'up',
+        'down',
+        'drag-left',
+        'drag-middle',
+        'drag-right'
+    ];
+    for(var i = 0 ; i < pointerCallbacks.length ; i++) {
+        var callbackType = pointerCallbacks[i];
+        if(setup[callbackType]) {
+            pointerInput.addCallback(callbackType, setup[callbackType].bind(setup));
+        }
+    }
+    if(setup.keydown) {
+        document.addEventListener('keydown', setup.keydown.bind(setup));
+    }
+    if(setup.keyup) {
+        document.addEventListener('keyup', setup.keyup.bind(setup));
+    }
+    var fps = setup.fps || defaults.fps;
+    var update = setup.update || defaults.update;
+    var draw = setup.draw || defaults.draw;
+    var resetState = setup.resetState || defaults.resetState;
+    var state = resetState.call(setup, setup.state || defaults.state);
+    var events = setup.events || defaults.events;
+    var ctx = canvas.getContext('2d');
+    var lastDraw = null;
 
-    var update = setup.update;
-    var draw = setup.draw;
-    var state;
-    state = setup.state || {};
 
     if(setup.images) {
-        var imageNames = Object.keys(setup.images),
-            images = {},
-            loaded = 0,
-            total = imageNames.length,
-            load = function() {
-                loaded += 1;
-                if(loaded === total) {
-                    if(setup.events.loaded) {
-                        setup.events.loaded(images);
-                    }
-                    reqAnimFrame(drawLoop);
-                } else {
-                    if(setup.events.progress) {
-                        setup.events.progress((loaded / total) * 100.0);
-                    }
+        var imageNames = Object.keys(setup.images);
+        var images = {};
+        var loaded = 0;
+        var total = imageNames.length;
+        var load = function() {
+            loaded += 1;
+            if(loaded === total) {
+                if(events.loaded) {
+                    events.loaded(images);
                 }
-            };
+                reqAnimFrame(drawLoop);
+            }
+            else {
+                if(events.progress) {
+                    events.progress((loaded / total) * 100.0);
+                }
+            }
+        };
+        var error = function() {
+            alert('error loading images');
+        }
         for(var i = 0 ; i < imageNames.length ; i++) {
             images[imageNames[i]] = new Image;
             images[imageNames[i]].onload = load;
-            images[imageNames[i]].onerror = load;
-            images[imageNames[i]].src = 'images/' + imageNames[i] + '.png';
+            images[imageNames[i]].onerror = error;
+            images[imageNames[i]].src = setup.images[imageNames[i]];
         }
     }
     else {
         reqAnimFrame(drawLoop);
     }
+
     function drawLoop(time) {
         if(lastDraw === null) {
             lastDraw = time;
         }
         if(!time || time - lastDraw > 1000 / fps) {
             lastDraw = time;
-            if(update) {
-                update(state);
-            }
-            if(draw) {
-                draw(ctx, images, state);
-            }
+            update.call(setup, state);
+            draw.call(setup, ctx, images, state);
         }
         reqAnimFrame(drawLoop);
     }
--- example.js
+++ example.js
@@ -3,9 +3,32 @@ window.onload = function() {
         width: 200,
         height: 300,
         canvas: document.getElementById('canvas'),
+        state: {
+            pos: {x: 50, y: 0},
+            direction: {x: 0, y: 0}
+        },
+        keydown: {
+            'F': function(state) {
+                state.direction.x = -1;
+            },
+            'G': function(state) {
+                state.direction.y = 1;
+            },
+            'H': function(state) {
+                state.direction.x = 1;
+            },
+            'T': function(state) {
+                state.direction.y = -1;
+            }
+        },
+        update: function(state) {
+            state.pos.x += state.direction.x;
+            state.pos.y += state.direction.y;
+        },
         draw: function(ctx, images, state) {
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
             ctx.fillStyle = '#0f0';
-            ctx.fillRect(0, 0, 50, 50);
+            ctx.fillRect(state.pos.x, state.pos.y, 50, 50);
             if(state.draw) {
             	state.draw(ctx);
             }
